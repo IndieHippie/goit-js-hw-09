93bdc90284fe2d70d2c49075d641af99016700cd
RomanStets <roma22stets@gmail.com>
2022-10-03 12:42:57 +0300

    startBtn, stopBtn

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,21 +1,21 @@
-
-const body = document.querySelector("body");
-const startBtn = document.querySelector("[data-start]");
-const stopBtn = document.querySelector("[data-stop]");
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 let timerId = null;
 
-startBtn.addEventListener("click", (evt) => {
-    timerId = setInterval(() => {
-        body.style.backgroundColor = getRandomHexColor();
-        evt.target.disabled = true;
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
     console.log(`random color  ${Math.random()}`);
   }, 1000);
 });
 
-
-stopBtn.addEventListener("click", () => {
-    clearInterval(timerId);
-    startBtn.disabled = false;
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
   console.log(`Interval with id ${timerId} has stopped!`);
 });
 
